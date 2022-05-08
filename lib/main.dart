f34e329b6520a91c22ea8ac3f0a751d18d788ebc
Usama Majid <83748784+Usamamajid13@users.noreply.github.com>
2022-05-08 15:35:13 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              "Welcome \nUser",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 38,
+                  color: Colors.black),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Sign up to join",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    color: Colors.grey[600]),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Name",
+                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+              ),
+            ),
+            const TextField(
+              textAlign: TextAlign.start,
+              decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  hintText: "John Doe"),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Email",
+                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+              ),
+            ),
+            const TextField(
+              textAlign: TextAlign.start,
+              decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  hintText: "Jenny@gmail.com"),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Password",
+                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+              ),
+            ),
+            const TextField(
+              obscureText: true,
+              textAlign: TextAlign.start,
+              decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  hintText: "******"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 25,
+                  height: 25,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.purple,
+                  ),
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Row(
+                  children: const [
+                    Text("I agree to the ",style: TextStyle(
+                         color: Colors.grey),),
+                    Text(
+                      "Terms of Service",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color(0xff3BCF93),
+                    borderRadius: BorderRadius.circular(15)),
+                width: MediaQuery.of(context).size.width,
+                height: 70,
+                padding: const EdgeInsets.all(10),
+                child: const Center(
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text("Have an account? ",style: TextStyle(
+                  fontSize: 16,
+                    color: Colors.grey),),
+                Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontSize: 16,
+                      fontWeight: FontWeight.w600, color: Colors.black),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
